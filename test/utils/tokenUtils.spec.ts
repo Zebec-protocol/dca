943365b17a41d2671cae53c2e37199901cba641b
Ashish Sapkota <aacsspkt3@gmail.com>
2022-06-16 17:15:37 +0545

    test token utils

---
+++ test/utils/tokenUtils.spec.ts
@@ -0,0 +1,22 @@
+import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
+import { expect } from "chai";
+import { describe } from "mocha";
+import { getMintInfo } from "../../src/utils";
+
+describe("tokenUtils unit test", () => {
+
+    describe('getMintInfo()', () => {
+        it("gets expected mint authority and decimals", async () => {
+            const expectedMintAuthority = new PublicKey("2wmVCSfPxGPjrnMMn7rchp4uaeoTqN39mXFC2zhPdri9");
+            const expectedDecimal = 6;
+
+            const reality = await getMintInfo(
+                new Connection(clusterApiUrl("mainnet-beta")),
+                new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v")
+            );
+            expect(reality.mintAuthority).to.deep.equal(expectedMintAuthority);
+            expect(reality.decimals).to.equal(expectedDecimal);
+        });
+
+    })
+});
