957a417628e900745254f9c7a6838e156087747a
Ashish Sapkota <aacsspkt3@gmail.com>
2022-06-16 17:15:27 +0545

    test pda utils

--- test/utils/pdaUtils.spec.ts
+++ test/utils/pdaUtils.spec.ts
@@ -1,9 +1,9 @@
 import { PublicKey } from "@solana/web3.js";
 import { expect } from "chai";
-import { findProgramAddress, findAssociatedTokenAddress } from "../../src/utils/pdaUtils";
+import { findProgramAddress, findAssociatedTokenAddress } from "../../src/utils";
 
 describe('pda utils unit test', () => {
-    describe("findProgramAddress", () => {
+    describe("findProgramAddress()", () => {
         it("generates public key and a bump seed", async () => {
             const pubkey1 = new PublicKey("8wFNccoRA12vLJ2JcRSc4PXFnYjUCGsVRkUGFUYBDk3P");
             const pubkey2 = new PublicKey("AqP5WP8QzYy1YV6gEwACevuBNZJv7SbaW3vsb3mupH6a");
@@ -18,7 +18,7 @@ describe('pda utils unit test', () => {
         })
     })
 
-    describe('findAssociatedTokenAddress', () => {
+    describe('findAssociatedTokenAddress()', () => {
         it("generates associated token program address", async () => {
             const owner = new PublicKey("9sxUjP243VNGmFY8HqmcUTtP4CPHJNQ6LWeM81iJ6nWx");
             const mint = new PublicKey("BEcGFQK1T1tSu3kvHC17cyCkQ5dvXqAJ7ExB2bb5Do7a")
