1d6e203f3b2ff1e838cb20301858e05ea8be47e9
Ashish Sapkota <aacsspkt3@gmail.com>
2022-06-16 17:17:23 +0545

    Specify exports in  entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export * from "./instruction";
+export * from "./constants";
+export * from "./instruction/index";
+export * from "./models/dcaAccount";
+export * from "./utils/index";
+
 
 console.log("Its running");
