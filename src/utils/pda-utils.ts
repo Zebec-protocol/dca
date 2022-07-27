1838b503c8cfa88eeeef35002ec62fb487ac9deb
Ashish Sapkota <aacsspkt3@gmail.com>
2022-07-27 08:26:31 +0545

    Update findVaultAddress

--- src/utils/pda-utils.ts
+++ src/utils/pda-utils.ts
@@ -1,4 +1,7 @@
-import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import {
+	ASSOCIATED_TOKEN_PROGRAM_ID,
+	TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
 import { PublicKey } from "@solana/web3.js";
 
 import { DCA_PROGRAM_ID } from "../constants";
@@ -7,8 +10,8 @@ export async function findProgramAddress(seeds: Buffer[], programId = DCA_PROGRA
 	return await PublicKey.findProgramAddress(seeds, programId);
 }
 
-export async function findVaultAddress(owner: PublicKey, dcaAccount: PublicKey): Promise<PublicKey> {
-	return (await findProgramAddress([owner.toBuffer(), dcaAccount.toBuffer()]))[0];
+export async function findVaultAddress(owner: PublicKey): Promise<PublicKey> {
+	return (await findProgramAddress([owner.toBuffer()]))[0];
 }
 
 export async function findAssociatedTokenAddress(owner: PublicKey, mint: PublicKey): Promise<PublicKey> {
