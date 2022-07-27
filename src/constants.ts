40aea63c3ee623f78bfeba18224e87d554679031
Ashish Sapkota <aacsspkt3@gmail.com>
2022-07-27 09:44:28 +0545

    Update connection constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,19 @@
-import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
+import {
+	Cluster,
+	clusterApiUrl,
+	Connection,
+	PublicKey,
+} from "@solana/web3.js";
 
 /** The dca program id. */
 export const DCA_PROGRAM_ID = new PublicKey("89U3HCacYnqJYUX33EupQRyKLBAqA9vb6tzAATRp19c7");
 
 /** Json RPC client to communicate with Solana blockchain. */
-export const CONNECTION = new Connection(clusterApiUrl("mainnet-beta"));
+export const CONNECTION: { [cluster in Cluster]: Connection } = {
+	"mainnet-beta": new Connection(clusterApiUrl("mainnet-beta")),
+	devnet: new Connection(clusterApiUrl("devnet")),
+	testnet: new Connection(clusterApiUrl("testnet")),
+};
 
 /** The devnet raydium liquidity pool program v4 id. */
 export const DEVNET_LIQUIDITY_PROGRAM_ID_V4 = new PublicKey("9rpQHSyFVM1dkkHFQ2TtTzPEW7DVmEyPmN8wVniqJtuC");
